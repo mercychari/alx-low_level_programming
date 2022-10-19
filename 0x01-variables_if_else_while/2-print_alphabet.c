f2a74edecded6f19ac69250db5afe33078cb2b81
mercychari <mmwanyasi@gmail.com>
2022-10-19 22:27:38 +0000

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,11 @@
  */
 int main(void)
 {
-	char letter;
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
+	for (i = 0; i < 26; i++)
+		putchar(alp[i]);
 
 	{
 	putchar('\n');
