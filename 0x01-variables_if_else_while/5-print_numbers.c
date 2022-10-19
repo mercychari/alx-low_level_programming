79eed5894a2f78c0912c6977892914ec9b2e2abe
mercychari <mmwanyasi@gmail.com>
2022-10-19 23:43:19 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stio.h>
+
+/**
+ * main - Prints numbers from 0 to 9
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
