9bf9b4b78aa85733e6b7247f6fbb0c1361d1d58d
mercychari <mmwanyasi@gmail.com>
2022-10-20 00:04:23 +0000

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints reverse alphabet
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
